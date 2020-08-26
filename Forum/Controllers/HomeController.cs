f6477fd15c5d8e341a1caed97144479adb5013bb
Alex <alexeyvolskiy@gmail.com>
2020-08-26 11:35:03 +0300

    initial commit

---
+++ Forum/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Forum.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace Forum.Controllers
+{
+	public class HomeController : Controller
+	{
+		MobileContext db;
+		public HomeController(MobileContext context)
+		{
+			db = context;
+		}
+		public IActionResult Index()
+		{
+			return View(db.Phones.ToList());
+		}
+
+		public IActionResult Orders()
+		{
+			return View(db.Orders.ToList());
+		}
+
+		[HttpGet]
+		public IActionResult Buy(int? id)
+		{
+			if (id == null)
+				RedirectToAction("Index");
+
+			ViewBag.PhoneId = id;
+			return View();
+		}
+
+		[HttpPost]
+		public string Buy(Order order)
+		{
+			db.Orders.Add(order);
+			db.SaveChanges();
+			return $"Thanks for shoping with us {order.User}!";
+		}
+	}
+}
---
+++ Forum/Models/Message.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Models
+{
+	public class Message
+	{
+		private Guid userId;
+		private string text;
+		private DateTime creatingDate;
+		public Message(Guid userId, string text)
+		{
+			this.userId = userId;
+			this.text = text;
+			creatingDate = DateTime.Now;
+		}
+	}
+}
