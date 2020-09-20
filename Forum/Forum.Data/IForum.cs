b81aa91580d6b478d0c09049f912705158f31a7a
Alex <alexeyvolskiy@gmail.com>
2020-09-20 13:55:03 +0300

    Added interface for working with dbContext, and service which impemented it

---
+++ Forum/Forum.Data/IForum.cs
@@ -0,0 +1,20 @@
+﻿using Forum.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Forum.Data
+{
+	public interface IForum
+	{
+		Models.Forum GetForum(int id);
+		IEnumerable<Models.Forum> GetAll();
+		IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+		Task Create(Models.Forum forum);
+		Task Delete(int forumId);
+		Task UpdateForumTitle(int forumId, string newTitle);
+		Task UpdateForumDescription(int forumId, string newDescription);
+	}
+}
---
+++ Forum/Forum.Service/ForumService.cs
@@ -0,0 +1,54 @@
+﻿using Forum.Data;
+using Forum.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Forum.Service
+{
+	public class ForumService : IForum
+	{
+		private readonly ApplicationDbContext _context;
+
+		public ForumService(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+
+		public Task Create(Data.Models.Forum forum)
+		{
+			return new Task(_context.Forums.Add(forum));
+		}
+
+		public Task Delete(int forumId)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<Data.Models.Forum> GetAll()
+		{
+			return _context.Forums.Include(forum => forum.Posts);
+		}
+
+		public IEnumerable<ApplicationUser> GetAllActiveUsers()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Data.Models.Forum GetForum(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateForumDescription(int forumId, string newDescription)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task UpdateForumTitle(int forumId, string newTitle)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
